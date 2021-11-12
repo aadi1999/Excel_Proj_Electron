be7b842aefe1a2056ba9670310cc925be7bc3009
Aadi Shakti Tripathi <aadi1999>
2021-11-12 23:02:02 +0530

    database(db) value is updated

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const $ = require("jquery")
 
 $("document").ready(function(){
-    
+    // a click event is attached on element with cell class
+    let db;
+
     $(".cell").on("click", function(){
         console.log(this);
         let rowId = Number($(this).attr("rowid"));
@@ -9,5 +11,42 @@ $("document").ready(function(){
         let cellAddress = String.fromCharCode(65+colId) + rowId;
         console.log(cellAddress);
 
+        $("#address").val(cellAddress);
+
     })
+
+
+    $(".cell").on("blur", function(){
+            // console.log(this);
+            console.log("blur event fired");
+            let val =  $(this).text();
+            let rowId = Number($(this).attr("rowid"));
+            let colId = Number($(this).attr("colid"));
+            let cellObject = db[rowId][colId];
+            console.log(cellObject)
+            cellObject.value = val;
+            // console.log(val);
+    })
+
+    function init(){
+        // db = 26*100
+
+        db = []; // initiliase database with empty array
+
+        for(let i = 0; i<100; i++){
+            let row = []; // this represent the single row
+            for(let j = 0; j < 26; j++){
+                let cellAddress = String.fromCharCode(65+j) + (i +1);
+                let cellObject = {
+                     name: cellAddress,
+                     value:"",
+                     address:""
+                 }
+                 row.push(cellObject); // cell object is pushed 26 times
+            }
+            db.push(row);
+        }
+        console.log(db);
+    }
+    init();
 })
